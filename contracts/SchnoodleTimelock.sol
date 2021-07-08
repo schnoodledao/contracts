c87427af881ed385706127b282a59541795d7ea6
Jason Payne <github@x-treem.net>
2021-07-08 23:29:35 +0200

    Downgrade OpenZeppelin Contracts and Solidity versions
    There is a bug or compatibility issue using OpenZeppelin Contracts 4.1 and Solidity 0.8.6 which results in minimal proxies to not be verified on Etherscan.

--- contracts/SchnoodleTimelock.sol
+++ contracts/SchnoodleTimelock.sol
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import "@openzeppelin/contracts/token/ERC20/utils/TokenTimelock.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/TokenTimelockUpgradeable.sol";
 
--- truffle-config.js
+++ truffle-config.js
@@ -46,7 +46,7 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "^0.8.0",
+      version: "0.8.5",
       settings: {
         optimizer: {
           enabled: true,
