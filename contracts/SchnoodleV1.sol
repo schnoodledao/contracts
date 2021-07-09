5238698b57d7e9b40e6d4f3c3aef5ca7c7f508a4
Jason Payne <github@x-treem.net>
2021-07-10 01:59:02 +0200

    Add Initializable import to Schnoodle contract

--- contracts/SchnoodleV1.sol
+++ contracts/SchnoodleV1.sol
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/presets/ERC20PresetFixedSupplyUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
