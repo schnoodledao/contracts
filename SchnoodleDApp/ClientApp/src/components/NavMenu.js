03330bc42fb3c04583a09ce69116fbb2de1d1e3e
Jason Payne <github@x-treem.net>
2021-10-20 19:47:35 +0200

    Improve contract migration process for development environment

--- SchnoodleDApp/ClientApp/src/components/NavMenu.js
+++ SchnoodleDApp/ClientApp/src/components/NavMenu.js
@@ -27,26 +27,26 @@ export class NavMenu extends Component {
         <Navbar className="navbar mb-2 shadow-lg bg-neutral text-neutral-content" light>
           <Container className="m-auto">
             <div class="flex-1 px-2 mx-2">
-    <span class="text-lg font-bold">
-            <NavbarBrand tag={Link} to="/">Schnoodle X</NavbarBrand>
-            </span>
-            </div>
-            <div class="flex-none hidden px-2 mx-2 lg:flex">
-    <div class="flex items-stretch">
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem className="btn btn-ghost  rounded-btn">
-                  <NavLink tag={Link} className="" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem className="btn btn-ghost rounded-btn">
-                  <NavLink tag={Link} className="" to="/staking">Staking</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-            <div class="flex-none">
-            <NavbarToggler onClick={this.toggleNavbar} className="btn btn-square btn-ghost" />
-            </div>
-            </div>
+              <span class="text-lg font-bold">
+                <NavbarBrand tag={Link} to="/">Schnoodle X</NavbarBrand>
+              </span>
+              </div>
+              <div class="flex-none hidden px-2 mx-2 lg:flex">
+                <div class="flex items-stretch">
+                  <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                    <ul className="navbar-nav flex-grow">
+                      <NavItem className="btn btn-ghost  rounded-btn">
+                        <NavLink tag={Link} className="" to="/">Home</NavLink>
+                      </NavItem>
+                      <NavItem className="btn btn-ghost rounded-btn">
+                        <NavLink tag={Link} className="" to="/staking">Staking</NavLink>
+                      </NavItem>
+                    </ul>
+                  </Collapse>
+                <div class="flex-none">
+                  <NavbarToggler onClick={this.toggleNavbar} className="btn btn-square btn-ghost" />
+                </div>
+              </div>
             </div>
           </Container>
         </Navbar>
--- SchnoodleDApp/ClientApp/tailwind.config.js
+++ SchnoodleDApp/ClientApp/tailwind.config.js
@@ -14,13 +14,13 @@ module.exports = {
     },
     plugins: [
         require('daisyui'),
-      ],
-      daisyui: {
-        styled: true,
-        themes: true,
-        base: true,
-        utils: true,
-        logs: true,
-        rtl: false,
-      },
+    ],
+    daisyui: {
+      styled: true,
+      themes: true,
+      base: true,
+      utils: true,
+      logs: true,
+      rtl: false,
+    },
 }
