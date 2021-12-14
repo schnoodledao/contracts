210fd10e64f28212dce9ac77853e640792106880
howlermonkey <iain@whichbritishschool.com>
2021-12-14 15:14:31 +0000

    cursor-pointer style fix

--- SchnoodleDApp/ClientApp/src/components/MoonControl.js
+++ SchnoodleDApp/ClientApp/src/components/MoonControl.js
@@ -295,7 +295,7 @@ export class MoonControl extends Component {
             const pendingBlocks = getPendingBlocks(depositInfo.deposit, this.state.blockNumber);
             return (
               <div role="row" key={depositInfo.deposit.blockNumber}>
-                <span role="cell" data-header={resources.FARMING_OVERVIEW.ACCOUNT.TITLE + ":"} class="border-l-0" onClick={() => this.showMoonFarm(depositInfo.account)} style={{ cursor: 'pointer' }}>{depositInfo.account}</span>
+                <span role="cell" data-header={resources.FARMING_OVERVIEW.ACCOUNT.TITLE + ":"} class="border-l-0 cursor-pointer" onClick={() => this.showMoonFarm(depositInfo.account)}>{depositInfo.account}</span>
                 <span role="cell" data-header={resources.FARMING_SUMMARY.BLOCK_NUMBER.TITLE + ":"} class="border-l-0">{depositInfo.deposit.blockNumber}</span>
                 <span role="cell" data-header={resources.FARMING_SUMMARY.DEPOSIT_AMOUNT.TITLE + ":"}>{amount.toLocaleString()}</span>
                 <span role="cell" data-header={resources.FARMING_SUMMARY.PENDING_BLOCKS.TITLE + ":"} title={blocksDurationText(pendingBlocks)}>{pendingBlocks}</span>
