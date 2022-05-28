9c80c487e1c3b4d89d66b5ac6aa53b5badf0bb8a
koteakhaladze <koteakh@gmail.com>
2022-05-28 20:30:49 +0300

    add line break

--- SchnoodleDApp/ClientApp/src/components/Bridge.jsx
+++ SchnoodleDApp/ClientApp/src/components/Bridge.jsx
@@ -10,6 +10,7 @@ import { initializeHelpers, handleError, getWeb3, scaleUpUnits, scaleDownUnits, 
 import Web3 from 'web3';
 import Select from 'react-select';
 
+
 const bigInt = require('big-integer');
 const Network = createEnum(['ethereum', 'bsc']);
 // ReSharper restore InconsistentNaming
@@ -125,6 +126,7 @@ export default class Bridge extends Component {
 
       await this.changeTargetNetwork({ value: localStorage.getItem('targetNetwork') ?? sourceNetwork });
       await this.changeSourceNetwork({ value: localStorage.getItem('sourceNetwork') ?? sourceNetwork });
+
       this.setState({
         web3,
         networkId,
