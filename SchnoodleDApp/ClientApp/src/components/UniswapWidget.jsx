02d5b9ad27ac7c6bffafc36cb82d262d4c16c538
tnrdd <tnrdd@tutanota.com>
2022-07-05 16:14:43 +0100

    change fonts for better readability

--- SchnoodleDApp/ClientApp/src/components/UniswapWidget.jsx
+++ SchnoodleDApp/ClientApp/src/components/UniswapWidget.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { SwapWidget } from '@uniswap/widgets/dist/index.js';
+import '@uniswap/widgets/dist/fonts.css';
 
 const uniswapTokenList = 'https://tokens.coingecko.com/uniswap/all.json';
 const nativeToken = 'NATIVE';
@@ -9,14 +10,15 @@ const schnoodleToken = '0xD45740aB9ec920bEdBD9BAb2E863519E59731941';
 const theme = {
   primary: '#ffffff',
   secondary: '#adb5bd',
-  container: '#8d38d7',
-  module: '#1d1249',
-  interactive: '#f0ca30',
+  container: '#8a5af5',
+  module: '#170C36',
+  interactive: '#f3cc30',
   accent: '#b42a6f',
   outline: '#ced4da',
   dialog: '#1d1249',
   error: '#dc3545',
   success: '#8d38d7',
+  fontFamily: 'Verdana',
 };
 
 export default class UniswapWidget extends Component {
