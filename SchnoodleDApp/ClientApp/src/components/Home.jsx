eb2622f2c06f054983ac177d3d8a0a8f1121ec93
Charcosset Antoine <antoine.charcosset@sfr.fr>
2022-07-08 17:01:21 +0200

    Uniswap widget

--- SchnoodleDApp/ClientApp/src/components/Home.jsx
+++ SchnoodleDApp/ClientApp/src/components/Home.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import { general, farming } from '../resources';
+import { SwapWidget } from '@uniswap/widgets';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
+
+    const uniswapTokenList = 'https://tokens.coingecko.com/uniswap/all.json';
+    const SNOOD = '0xd45740ab9ec920bedbd9bab2e863519e59731941';
+    const theme = {
+      primary: '#ffffff',
+      secondary: '#ffffff',
+      interactive: '#AD73B1',
+      container: '#702DDC',
+      module: '#20134E',
+      accent: '#9999CC',
+      outline: '#CC1',
+      dialog: '#140A2E',
+      fontFamily: 'Josefin Sans',
+      borderRadius: 1
+    }
+
     return (
       <div className="tw-overflow-hidden tw-antialiased tw-font-roboto tw-mx-4">
         <div className="h-noheader md:tw-flex">
@@ -17,6 +34,15 @@ export class Home extends Component {
               <a href="/farming">
                 <button className="tw-px-4 tw-py-2 tw-mt-4 tw-text-lg tw-text-accent tw-border-accent tw-duration-200 tw-transform tw-border tw-rounded-lg hover:tw-bg-purple-100 focus:tw-outline-none">{farming.START_FARMING}</button>
               </a>
+                 <div className="tw-flex tw-justify-center tw-m-10 ">
+                        <SwapWidget
+                          provider={window.web3.currentProvider}
+                         jsonRpcEndpoint={process.env.REACT_APP_ETH_URL}
+                         tokenList={uniswapTokenList}
+                         theme={theme}
+                         defaultInputTokenAddress={SNOOD}
+                        />
+                  </div>
             </div>
           </div>
         </div>
